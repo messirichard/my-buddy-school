93301cc46442c67ec8d8a1ed7d9818ea0ac39320
Richard <richardofay@gmail.com>
2020-09-23 20:12:00 +0700

    Progress

---
+++ assets/js/dist/index.dev.js
@@ -0,0 +1,17 @@
+"use strict";
+
+$('.slick-carousel').slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows: true
+});
+$('.buttonsss').first().addClass('active');
+$('.buttonsss').click(function () {
+  var $this = $(this);
+  $siblings = $this.parent().children(), position = $siblings.index($this);
+  console.log(position);
+  $('.fills div').removeClass('active').eq(position).addClass('active');
+  $siblings.removeClass('active');
+  $this.addClass('active');
+});
