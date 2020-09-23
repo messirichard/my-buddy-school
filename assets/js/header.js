3869140c05bc4d1615834bf7a2ca23167e836d40
DeniJuliSetiawan112 <denijuli.setiawan@gmail.com>
2020-09-23 07:57:12 +0700

    header

---
+++ assets/js/header.js
@@ -0,0 +1,8 @@
+// import Heads from '../../page/Layouts/heads.html';
+// class MyComponent extends HTMLElement {
+//     connectedCallback() {
+//       this.innerHTML = Heads;
+//     }
+// }
+      
+// customElements.define('heads', MyComponent);
