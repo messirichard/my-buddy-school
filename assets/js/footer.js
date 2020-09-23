6d546b22f2d8b7b7886dc134aafa38b03992985c
DeniJuliSetiawan112 <denijuli.setiawan@gmail.com>
2020-09-23 21:29:45 +0700

    footer and header

---
+++ assets/js/footer.js
@@ -0,0 +1,61 @@
+class Footer extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = `
+        <div class="p-5 pref-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-md-4 col-12">
+                        <div class="logo mb-4">
+                            <img src="../../assets/img/logo-head.png" alt="logo" height="35">
+                        </div>
+                        <p class="about-us mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tellus vitae facilisi consequat donec turpis aliquam at. Egestas arcu ut.</p>
+                        <ul class="social-media d-flex align-items-center p-0">
+                            <li class="item">
+                                <a href="#" class="link p-2 d-inline-block"><i class="fab fa-youtube"></i></a>
+                            </li>
+                            <li class="item ml-2">
+                                <a href="#" class="link p-2 d-inline-block"><i class="fab fa-facebook-square"></i></a>
+                            </li>
+                            <li class="item ml-2">
+                                <a href="#" class="link p-2 d-inline-block"><i class="fab fa-twitter"></i></a>
+                            </li>
+                            <li class="item ml-2">
+                                <a href="#" class="link p-2 d-inline-block"><i class="fab fa-instagram"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-12 mt-3">
+                        <ul class="footer-link p-0">
+                            <li class="item text-bold">My Buddy School</li>
+                            <li class="item"><a href="">About Us</a></li>
+                            <li class="item"><a href="">Contact Us</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-12 mt-3">
+                        <ul class="footer-link p-0">
+                            <li class="item text-bold">Menu</li>
+                            <li class="item"><a href="">Dashboard</a></li>
+                            <li class="item"><a href="">Course</a></li>
+                            <li class="item"><a href="">My Course</a></li>
+                            <li class="item"><a href="">Inbox</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-12 mt-3">
+                        <ul class="footer-link p-0">
+                            <li class="item text-bold">For users</li>
+                            <li class="item"><a href="">Login</a></li>
+                            <li class="item"><a href="">Register</a></li>
+                            <li class="item"><a href="">Account</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer class="footer py-2">
+            <p class="text-white text-center m-0">© 2020 My Buddy School.</p>
+        </footer>
+    `;
+    }
+}
+      
+customElements.define('footer-component', Footer);
--- assets/js/header.js
+++ assets/js/header.js
@@ -1,8 +1,239 @@
-// import Heads from '../../page/Layouts/heads.html';
-// class MyComponent extends HTMLElement {
-//     connectedCallback() {
-//       this.innerHTML = Heads;
-//     }
-// }
+class HeaderAfter extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = `
+        <div class="drawer drawer-left slide" tabindex="-1" role="dialog" aria-labelledby="drawer-1-title" aria-hidden="true" id="drawer-1">
+            <div class="drawer-content drawer-content-scrollable" role="document">
+                <div class="drawer-header">
+                    <h4 class="drawer-title" id="drawer-1-title">
+                        <img src="../../assets/img/logo-head.png" alt="logo" height="25">
+                    </h4>
+                </div>
+                <div class="drawer-body">
+                    <form class="form-inline my-2 my-lg-0 search-form">
+                        <div class="input-group w-100">
+                            <input type="text" class="form-control input-search" placeholder="reading" name="key">
+                            <div class="input-group-append">
+                                <button class="btn-search">
+                                    <img src="../../assets/img/search.png" alt="">
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+
+                    <ul class="navbar-nav border-top mt-2">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link text-primary pt-2 pb-2"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link text-primary pt-2 pb-2"><i class="fas fa-book-open"></i> Course</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link text-primary pt-2 pb-2"><i class="fas fa-user-graduate"></i> My Course</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link text-primary pt-2 pb-2"><i class="fas fa-user"></i> Profile</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link text-primary pt-2 pb-2"><i class="fas fa-comment-alt"></i> Message</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link text-primary pt-2 pb-2"><i class="fas fa-sign-out-alt"></i> Logout</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="drawer-footer">
+                    <div class="profile d-flex flex-row align-items-first">
+                        <div class="img-profile">
+                            <img src="../../assets/img/profile.jpg" alt="profile" class="img-fit rounded">
+                        </div>
+                        <div class="detail ml-1">
+                            <p class="text-bold">Hi, Bambang</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light m-navbar">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="../../assets/img/logo-head.png" alt="logo">
+                </a>
+                <button class="navbar-toggler" data-toggle="drawer" data-target="#drawer-1">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                <div class="d-flex flex-row align-items-center justiy-content-between w-100">
+                    <div class="collapse navbar-collapse">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item">
+                                <a href="#" class="nav-link text-black d-flex align-items-center">Dashboard</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link text-black d-flex align-items-center">Course</a>
+                            </li>
+                        </ul>
+
+                        <form class="form-inline my-2 my-lg-0 search-form">
+                            <div class="input-group w-100">
+                                <input type="text" class="form-control input-search" placeholder="reading" name="key">
+                                <div class="input-group-append">
+                                    <button class="btn-search">
+                                        <img src="../../assets/img/search.png" alt="">
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item d-flex align-items-center">
+                                <a href="#" class="nav-link text-black">My Course</a>
+                            </li>
+                            <li class="nav-item d-flex align-items-center">
+                                <a href="#" class="nav-link">
+                                    <img src="../../assets/img/mail.png" alt="mail">
+                                    <span class="badge badge-pill badge-warning text-white badge-mail">1</span>
+                                </a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a href="#" class="nav-link d-flex flex-row align-items-center dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <p class="m-0 text-black">Hi, Derry</p>
+                                    <img src="../../assets/img/profile.jpg" alt="profile" class="img-fit ml-1 rounded-circle">
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">Profile</a>
+                                    <a class="dropdown-item" href="#">My Course</a>
+                                    <a class="dropdown-item" href="#">Message</a>
+                                    <a class="dropdown-item text-danger" href="#">Logout</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </nav>
+    `;
+    }
+}
+class HeaderBefore extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = `
+        <div class="drawer drawer-left slide" tabindex="-1" role="dialog" aria-labelledby="drawer-1-title" aria-hidden="true" id="drawer-1">
+            <div class="drawer-content drawer-content-scrollable" role="document">
+                <div class="drawer-header">
+                    <h4 class="drawer-title" id="drawer-1-title">
+                        <img src="../../assets/img/logo-head.png" alt="logo" height="25">
+                    </h4>
+                </div>
+                <div class="drawer-body">
+                    <form class="form-inline my-2 my-lg-0 search-form">
+                        <div class="input-group w-100">
+                            <input type="text" class="form-control input-search" placeholder="reading" name="key">
+                            <div class="input-group-append">
+                                <button class="btn-search">
+                                    <img src="../../assets/img/search.png" alt="">
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+
+                    <ul class="navbar-nav border-top mt-2">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link text-primary pt-2 pb-2"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link text-primary pt-2 pb-2"><i class="fas fa-book-open"></i> Course</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link text-primary pt-2 pb-2"><i class="fas fa-user-graduate"></i> My Course</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link text-primary pt-2 pb-2"><i class="fas fa-user"></i> Profile</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link text-primary pt-2 pb-2"><i class="fas fa-comment-alt"></i> Message</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link text-primary pt-2 pb-2"><i class="fas fa-sign-out-alt"></i> Logout</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="drawer-footer">
+                    <div class="profile d-flex flex-row align-items-first">
+                        <div class="img-profile">
+                            <img src="../../assets/img/profile.jpg" alt="profile" class="img-fit rounded">
+                        </div>
+                        <div class="detail ml-1">
+                            <p class="text-bold">Hi, Bambang</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light m-navbar">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="../../assets/img/logo-head.png" alt="logo">
+                </a>
+                <button class="navbar-toggler" data-toggle="drawer" data-target="#drawer-1">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                <div class="d-flex flex-row align-items-center justiy-content-between w-100">
+                    <div class="collapse navbar-collapse">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item">
+                                <a href="#" class="nav-link text-black d-flex align-items-center">Dashboard</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link text-black d-flex align-items-center">Course</a>
+                            </li>
+                        </ul>
+
+                        <form class="form-inline my-2 my-lg-0 search-form">
+                            <div class="input-group w-100">
+                                <input type="text" class="form-control input-search" placeholder="reading" name="key">
+                                <div class="input-group-append">
+                                    <button class="btn-search">
+                                        <img src="../../assets/img/search.png" alt="">
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item d-flex align-items-center">
+                                <a href="#" class="nav-link text-black">My Course</a>
+                            </li>
+                            <li class="nav-item d-flex align-items-center">
+                                <a href="#" class="nav-link">
+                                    <img src="../../assets/img/mail.png" alt="mail">
+                                    <span class="badge badge-pill badge-warning text-white badge-mail">1</span>
+                                </a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a href="#" class="nav-link d-flex flex-row align-items-center dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <p class="m-0 text-black">Hi, Derry</p>
+                                    <img src="../../assets/img/profile.jpg" alt="profile" class="img-fit ml-1 rounded-circle">
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">Profile</a>
+                                    <a class="dropdown-item" href="#">My Course</a>
+                                    <a class="dropdown-item" href="#">Message</a>
+                                    <a class="dropdown-item text-danger" href="#">Logout</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </nav>
+    `;
+    }
+}
       
-// customElements.define('heads', MyComponent);
+customElements.define('header-after', HeaderAfter);
+customElements.define('header-before', HeaderBefore);
